fbfe71236848e322ec35421d87b3844843f11983
JDSalisbury <Salisbury.jeffery@gmail.com>
2018-05-02 09:48:41 -0400

    txt is being read

--- src/WordCounter.java
+++ src/WordCounter.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class WordCounter {
 
@@ -12,6 +14,14 @@ public class WordCounter {
 		Path fileFinder = Paths.get(System.getProperty("user.dir")).resolve(fileName);
 
 		BufferedReader fileReader = new BufferedReader(new FileReader(fileFinder.toFile()));
+
+		Map<String, Integer> occurences = new TreeMap<>();
+
+		String linedUp = fileReader.readLine();
+		while (linedUp != null) {
+			System.out.println(linedUp);
+			linedUp = fileReader.readLine();
+		}
 	}
 
 }
